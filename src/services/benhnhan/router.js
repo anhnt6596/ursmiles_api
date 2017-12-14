8881bf0b32af0f667eec2cdb5ab8b7d764025339
AnhTNguyen <anhnt6596@gmail.com>
2017-12-14 11:49:34 +0700

    .

--- src/services/benhnhan/router.js
+++ src/services/benhnhan/router.js
@@ -5,11 +5,10 @@ const router = express.Router();
 
 router.get('/',requireRole("admin"), getAll);
 
-// router.post('/create', requireRole("doctor"), create);
-// router.post('/create', create);
+// router.post('/edit/:MaSo', requireRole("doctor"), editbyMaSo);
+router.post('/edit/:MaSo', editbyMaSo);
 
-router.post('/edit/:MaSo', requireRole("doctor"), editbyMaSo);
-
-router.get('/getbyidbs/:IDBacSi', requireRole("doctor") ,getByIDBacSi);
+// router.get('/getbyidbs/:IDBacSi', requireRole("doctor"), getByIDBacSi);
+router.get('/getbyidbs/:IDBacSi', getByIDBacSi);
 
 export default router;
--- src/services/khamtrongmieng/router.js
+++ src/services/khamtrongmieng/router.js
@@ -3,11 +3,11 @@ import {getAll, create, editbyMaSo, requireRole} from './controller';
 
 const router = express.Router();
 
-router.get('/',requireRole("admin"), getAll);
+router.get('/', requireRole("admin"), getAll);
 
-router.post('/create',requireRole("doctor") ,create);
+router.post('/create', requireRole("doctor"), create);
 
-router.post('/edit/:MaSo',requireRole("doctor"), editbyMaSo);
+router.post('/edit/:MaSo', requireRole("doctor"), editbyMaSo);
 
 
 export default router;
